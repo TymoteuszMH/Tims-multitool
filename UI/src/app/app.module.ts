c4ab876a63542ccf3a2ed1666f9c528caaf4caf8
TymekH <tymek.huczsza@o2.pl>
2023-05-06 13:25:12 +0200

    T-mt v 0.7.1
    quick fix

--- UI/src/app/app.module.ts
+++ UI/src/app/app.module.ts
@@ -73,8 +73,6 @@ import { ServiceWorkerModule } from '@angular/service-worker';
     }),
     ServiceWorkerModule.register('ngsw-worker.js', {
       enabled: isDevMode(),
-      // Register the ServiceWorker as soon as the application is stable
-      // or after 30 seconds (whichever comes first).
       registrationStrategy: 'registerWhenStable:30000'
     }),
   ],
