658f8b73b858b0b874afaeb19aedd0e47a326449
TymekH <tymek.huczsza@o2.pl>
2023-05-08 13:05:14 +0200

    T-mt v 1.0.2
    button to ask for notifications

--- UI/src/app/components/home/home.component.ts
+++ UI/src/app/components/home/home.component.ts
@@ -9,8 +9,30 @@ import { LoginDataService } from 'src/app/services/logindata.service';
 export class HomeComponent {
   //username
   login = ""
+  //checks if user asked for permision
+  askedForNot:boolean = false;
+  info: boolean = false;
   //check current username
   ngDoCheck(){
     this.login = LoginDataService.username;
+    if (Notification.permission === 'granted') {
+      this.askedForNot = true;
+    }else if (Notification.permission === 'denied'){
+      this.askedForNot = true;
+      this.info = true;
+    }
+  }
+  //check for notification premission
+  requestNotificationPermission() {
+    if (Notification.permission !== 'granted') {
+      Notification.requestPermission().then(permission => {
+        if (permission === 'granted') {
+          this.askedForNot = true;
+        }else if (Notification.permission === 'denied'){
+          this.askedForNot = true;
+          this.info = true;
+        }
+      });
+    }
   }
 }
