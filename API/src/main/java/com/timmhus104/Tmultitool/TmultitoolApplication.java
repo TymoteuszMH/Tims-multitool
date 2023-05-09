0144f2b241743ddff483cef06e3c588ea30de510
TymekH <tymek.huczsza@o2.pl>
2023-05-09 17:17:06 +0200

    T-mt v 1.0.3
    button change

--- API/src/main/java/com/timmhus104/Tmultitool/TmultitoolApplication.java
+++ API/src/main/java/com/timmhus104/Tmultitool/TmultitoolApplication.java
@@ -17,10 +17,9 @@ public class TmultitoolApplication {
 	@Bean
 	public WebMvcConfigurer corsConfigurer()
 	{
-		String[] allowDomains = new String[3];
+		String[] allowDomains = new String[2];
 		allowDomains[0] = "http://localhost:4200";
-		allowDomains[1] = "http://192.168.1.102:8081";
-		allowDomains[2] = "http://localhost:8080";
+		allowDomains[1] = "http://localhost:8080";
 
 		System.out.println("CORS configuration....");
 		return new WebMvcConfigurer() {
